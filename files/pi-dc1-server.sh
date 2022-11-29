6fb74068a051f077e1974da93423c71a16dc1580
GoussCN <57469993+GoussCN@users.noreply.github.com>
2022-11-29 13:49:05 +0800

    Update pi-dc1-server.sh

--- files/pi-dc1-server.sh
+++ files/pi-dc1-server.sh
@@ -1,12 +1,12 @@
 #!/usr/bin/env bash
 
-JAR_URL="https://raw.githubusercontent.com/GoussCN/pi-dc1-server/main/files/dc1server.jar"
-SERVICE_URL="https://raw.githubusercontent.com/GoussCN/pi-dc1-server/main/files/dc1server.service"
+JAR_URL="https://github.com/GoussCN/pi-dc1-server/tree/main/files/dc1server.jar"
+SERVICE_URL="https://github.com/GoussCN/pi-dc1-server/tree/main/files/dc1server.service"
 
 #手动下载文件信息提示
 function echoManualDownloadInfo(){
     echo -e "\033[31m 文件下载失败，请登陆网页 https://github.com/GoussCN/pi-dc1-server/tree/main/files 手动下载 \033[0m"
-    echo -e "\033[31m 手动下载文件并放置到/opt/dc1server/目录下，再执行该脚本 \033[0m"
+    echo -e "\033[31m 手动下载文件并放置到/opt/目录下，再执行该脚本 \033[0m"
     echo -e "\033[31m 共需下载两个文件 dc1server.jar和dc1server.service \033[0m"
 }
 
@@ -15,8 +15,8 @@ function echoGreenInfo(){
 }
 
 function autoStart(){
-    cp /opt/dc1server/dc1server.service /etc/systemd/system/
-    rm -f /opt/dc1server/dc1server.service
+    cp /opt/dc1server.service /etc/systemd/system/
+    rm -f /opt/dc1server.service
 
     echo ""
     echo -n  "是否设置开机自动启动？【Y/N】"
@@ -30,41 +30,41 @@ function autoStart(){
     systemctl status dc1server.service
     echo ""
     echoGreenInfo "脚本执行完成，可输入命令 systemctl status dc1server.service 查看状态"
-    echoGreenInfo "手机apk请到 https://github.com/GoussCN/pi-dc1-server/tree/main/apk 下载"
+    echoGreenInfo "手机apk请到 https://github.com/GoussCN/pi-dc1-server/tree/main/files 下载"
 }
 
 #安装jdk，部署开机启动服务
 function install(){
-    if !(test -d /opt/dc1server/)
+    if !(test -d /opt/)
     then
-        mkdir /opt/dc1server/
+        mkdir /opt/
     fi
-	cd /opt/dc1server/
+	cd /opt/
 
-	if test -e /opt/dc1server/dc1server.jar
+	if test -e /opt/dc1server.jar
 	then
 	    echo ""
-	    echo -n  "是否删除原/opt/dc1server/dc1server.jar，重新下载？【Y/N】"
+	    echo -n  "是否删除原/opt/dc1server.jar，重新下载？【Y/N】"
         read  deleteOriJarFile
         if [[ ${deleteOriJarFile} == "Y" || ${deleteOriJarFile} == "y" ]]
         then
-            echoGreenInfo "删除原/opt/dc1server/dc1server.jar，重新下载"
-	        rm -f /opt/dc1server/dc1server.jar
+            echoGreenInfo "删除原/opt/dc1server.jar，重新下载"
+	        rm -f /opt/dc1server.jar
 	        wget ${JAR_URL}
         fi
     else
         wget ${JAR_URL}
 	fi
 
-	if test -e /opt/dc1server/dc1server.service
+	if test -e /opt/dc1server.service
 	then
 	    echo ""
-		echo -n  "是否删除原/opt/dc1server/dc1server.service，重新下载？【Y/N】"
+		echo -n  "是否删除原/opt/dc1server.service，重新下载？【Y/N】"
         read  deleteOriServiceFile
         if [[ ${deleteOriServiceFile} == "Y" || ${deleteOriServiceFile} == "y" ]]
         then
-            echoGreenInfo "删除原/opt/dc1server/dc1server.service，重新下载"
-	        rm -f /opt/dc1server/dc1server.service
+            echoGreenInfo "删除原/opt/dc1server.service，重新下载"
+	        rm -f /opt/dc1server.service
 	        wget ${SERVICE_URL}
         fi
     else
@@ -72,7 +72,7 @@ function install(){
 	fi
 	
 
-	if  test -e /opt/dc1server/dc1server.jar -a -e /opt/dc1server/dc1server.service
+	if  test -e /opt/dc1server.jar -a -e /opt/dc1server.service
 	then
         apt install -y default-jdk
 	    autoStart
@@ -84,7 +84,7 @@ function install(){
 #卸载
 function uninstall() {
     systemctl stop dc1server.service
-    rm -f /opt/dc1server/dc1server.jar
+    rm -f /opt/dc1server.jar
     rm -f /etc/systemd/system/dc1server.service
     echoGreenInfo "卸载完成！"
 }
